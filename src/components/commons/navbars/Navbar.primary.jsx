cbc18d84de27c3dc929d75b96dc09c3b261cd32d
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-03-22 03:55:50 +0500

    👌NEW: DYNAMIC ISLAND

--- src/components/commons/navbars/Navbar.primary.jsx
+++ src/components/commons/navbars/Navbar.primary.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { ButtonTransparent, IconPlain, Logo, PopoverUser, TextMd } from "..";
+import PopoverIsland from "../popover/Popover.island";
+import { AnimatePresence, motion } from "framer-motion";
 
 const NavbarPrimary = () => {
+  // States
+  const [isIslandOpen, setIsIslandOpen] = useState(false);
+
+  // Framer Motion Variants
+  const variants = {
+    popup: {
+      visible: {
+        scaleX: 1,
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.1 },
+      },
+      hidden: {
+        scaleX: 1.5,
+        opacity: 0,
+        y: 40,
+        transition: { duration: 0.1 },
+      },
+      exit: {
+        scaleX: 1.5,
+        opacity: 0,
+        y: 40,
+        transition: { duration: 0.1 },
+      },
+    },
+  };
+
   return (
-    <div className="py-5 px-8 border-b">
+    <nav className="py-5 px-8 border-b h-24">
       <div className="grid grid-cols-12 items-center justify-between">
-        <div className="col-span-4">
+        <div className="col-span-4 z-10">
           <Logo
             width={50}
             height={50}
@@ -13,20 +42,41 @@ const NavbarPrimary = () => {
             justify="justify-start"
           />
         </div>
-        <div className="col-span-4">
-          <div className="flex items-center justify-center shadow-md rounded-full p-2.5 w-full border-2">
-            <TextMd text="Anywhere" classes="px-4 border-r border-gray-300" />
-            <TextMd text="Any week" classes="px-4 border-r border-gray-300" />
-            <TextMd text="Add guests" classes="px-4 border-r" />
-            <IconPlain iconClass="fa-search" isRounded />
-          </div>
+        <div className="col-span-4 cursor-pointer">
+          <PopoverIsland
+            isIslandOpen={isIslandOpen}
+            setIsIslandOpen={setIsIslandOpen}
+          />
+          <AnimatePresence mode="wait" initial={false}>
+            {!isIslandOpen && (
+              <motion.div
+                variants={variants.popup}
+                animate="visible"
+                initial="hidden"
+                exit="exit"
+                onClick={() => setIsIslandOpen(true)}
+                className="flex items-center justify-center shadow-md rounded-full p-2.5 border-2"
+              >
+                <TextMd
+                  text="Anywhere"
+                  classes="px-4 border-r border-gray-300"
+                />
+                <TextMd
+                  text="Any week"
+                  classes="px-4 border-r border-gray-300"
+                />
+                <TextMd text="Add guests" classes="px-4 border-r" />
+                <IconPlain iconClass="fa-search" isRounded />
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
-        <div className="col-span-4 flex items-center justify-end gap-2">
+        <div className="col-span-4 flex items-center justify-end gap-2 z-10">
           <ButtonTransparent text="Airbnb your home" classes="rounded-full" />
           <PopoverUser />
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
---
+++ src/components/commons/popover/Popover.island.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const PopoverIsland = ({ setIsIslandOpen, isIslandOpen }) => {
+  // Framer Motion Variants
+  const variants = {
+    island: {
+      visible: {
+        height: "11rem",
+        transition: { duration: 0.2 },
+      },
+      hidden: {
+        height: "6rem",
+        transition: { duration: 0.2 },
+      },
+      exit: {
+        height: "0",
+        transition: { duration: 0.2 },
+      },
+    },
+    container: {
+      visible: {
+        scaleX: 1,
+        scaleY: 1,
+        y: 50,
+        opacity: "1",
+        transition: { duration: 0.2 },
+      },
+      hidden: {
+        scaleX: 0,
+        scaleY: 0,
+        y: -10,
+        opacity: "0",
+        transition: { duration: 0.2 },
+      },
+      exit: {
+        scaleX: 0,
+        scaleY: 0,
+        y: -10,
+        opacity: "0",
+        transition: { duration: 0.2 },
+      },
+    },
+  };
+
+  return (
+    <>
+      {isIslandOpen && (
+        <div
+          className="bg-[rgba(0,0,0,0.1)] h-screen w-screen absolute left-0 right-0 bottom-0 top-0"
+          onClick={() => setIsIslandOpen(false)}
+        ></div>
+      )}
+      <AnimatePresence>
+        {isIslandOpen && (
+          <motion.div
+            key="island"
+            variants={variants.island}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="bg-white h-44 top-0 text-center absolute left-0 w-full"
+          >
+            <motion.div key="child" variants={variants.container}>
+              Hello
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default PopoverIsland;
